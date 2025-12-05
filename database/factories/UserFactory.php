81549b72fead1058ce2e64941596bf8c8085c78a
Toby Nguyen <nktue2000@gmail.com>
2025-12-05 11:50:38 +0700

    v0.2.1: Tests fixed

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -34,7 +34,7 @@ class UserFactory extends Factory
             'two_factor_recovery_codes' => Str::random(10),
             'two_factor_confirmed_at' => now(),
 
-            'is_admin' => fake()->boolean(),
+            'is_admin' => fake()->randomElement(['TRUE', 'FALSE']),
         ];
     }
 
@@ -43,7 +43,7 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
@@ -53,7 +53,7 @@ class UserFactory extends Factory
      */
     public function withoutTwoFactor(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'two_factor_secret' => null,
             'two_factor_recovery_codes' => null,
             'two_factor_confirmed_at' => null,
